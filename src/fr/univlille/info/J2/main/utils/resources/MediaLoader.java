26b31648aec6b15ba86592152e11c43d9c464088
Camille Fourmaintraux <camille.fourmaintraux.etu@univ-lille.fr>
2023-12-24 17:29:42 +0000

    Début d'ajout d'effets sonores

---
+++ src/fr/univlille/info/J2/main/utils/resources/MediaLoader.java
@@ -0,0 +1,27 @@
+package fr.univlille.info.J2.main.utils.resources;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
+
+public class MediaLoader {
+	private static final String SOUND_DIRECTORY_PATH = "./res/sound/";
+	public static MediaLoader media_loader;
+	private MediaPlayer media_player;
+	
+	public void playSound(String name) {
+		System.out.println(SOUND_DIRECTORY_PATH+name);
+
+		System.out.println(new File(".").getAbsolutePath());
+		Media media = new Media(new File(SOUND_DIRECTORY_PATH+name).toURI().toString());
+		media_player = new MediaPlayer(media);
+		media_player.play();
+	}
+	
+	public static MediaLoader getMediaLoader() {
+		if(MediaLoader.media_loader==null) {
+			media_loader = new MediaLoader();
+		}
+		return media_loader;
+	}
+}
